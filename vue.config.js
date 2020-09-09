c966b9ed4e11f60a45962aa781bb184a62c673b7
Dallas Hoffman <dallashoff@gmail.com>
2020-09-09 19:03:55 -0400

    Removed PWA plugin; custom PWA setup later

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-    pwa: {
-        manifestOptions: {
-            name: 'Quic Calc',
-            short_name: 'Quic Calc'
-        }
-    },
     chainWebpack: (config) => {
         if (process.env.NODE_ENV === 'production') {
             config.plugin('html').tap((opts) => {
