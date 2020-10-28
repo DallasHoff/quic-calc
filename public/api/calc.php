45e952f19db32dd40f02487f98fd4658bd0a0b67
Dallas Hoffman <dallashoff@gmail.com>
2020-10-27 22:50:14 -0400

    calc endpoint setup

--- public/api/calc.php
+++ public/api/calc.php
@@ -1,2 +1,28 @@
 <?php
-http_response_code(501);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+try {
+    switch ($method) {
+
+        case 'GET':
+            // TODO
+            http_response_code(501);
+            exit();
+
+
+        case 'HEAD':
+            http_response_code(200);
+            exit();
+
+
+        default:
+            http_response_code(405);
+            exit();
+            
+
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    exit();
+}
