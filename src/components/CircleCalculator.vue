ba6df95301f70016c13f4402f3dac47187266da7
Dallas Hoffman <dallashoff@gmail.com>
2020-12-05 23:23:08 -0500

    Circle calc styling

--- src/components/CircleCalculator.vue
+++ src/components/CircleCalculator.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="circle-calculator">
-        <div class="circle-calculator__inner">
+        <section class="circle-calculator__inner">
             <h1 class="circle-calculator__heading">Area of a Circle</h1>
             <form class="circle-calculator__form" method="GET">
                 <label for="circle-radius">Circle Radius</label>
                 <input type="number" name="radius" id="circle-radius" step="any" v-model="radius">
-                <button type="submit">Calculate Area</button>
+                <div class="button-set">
+                    <button type="submit" class="button">Calculate Area</button>
+                </div>
             </form>
             <output class="circle-calculator__output" v-if="result">
                 Area = {{result}}
             </output>
-            <output class="circle-calculator__output" v-else-if="error">
+            <output class="circle-calculator__output circle-calculator__output--error" v-else-if="error">
                 Error: {{error}}
             </output>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -52,21 +54,12 @@ export default {
     &__heading {
         margin: 0 0 1rem 0;
     }
-    &__form {
-        /* TODO */
-        label {
-            display: block;
-        }
-        input {
-            display: block;
-        }
-        button {
-            display: block;
-        }
-    }
     &__output {
         display: block;
         margin: 1rem 0 0 0;
+        &--error {
+            color: #980000;
+        }
     }
 }
 </style>
